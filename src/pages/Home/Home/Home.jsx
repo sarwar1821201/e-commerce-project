1cc8256912229b27fc968a4b3934599cf533de6f
sarwar1821201 <sarwardipo1821201@gmail.com>
2025-08-03 16:15:54 +0600

    add banner image

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Banner from '../Banner/Banner'
 
 const Home = () => {
   return (
-    <div>This is Home</div>
+    <div>
+        <Banner></Banner>
+    </div>
   )
 }
 
