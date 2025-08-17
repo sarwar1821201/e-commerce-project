cb3b9c757be2601b396bd0a62a1742aed7124a73
sarwar1821201 <sarwardipo1821201@gmail.com>
2025-08-17 16:38:07 +0600

    add rating

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner'
 import Category from '../Category/Category'
 import PopularMenu from '../PopularMenu/PopularMenu'
 import Featured from '../Featured/Featured'
+import Testimonials from '../Testimonials/Testimonials'
 
 
 
@@ -13,6 +14,7 @@ const Home = () => {
         <Category></Category>
         <PopularMenu></PopularMenu>
         <Featured></Featured>
+        <Testimonials></Testimonials>
     </div>
   )
 }
---
+++ src/pages/Home/Testimonials/Testimonials.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
+
+const Testimonials = () => {
+
+    const [reviews, setReviews ] = useState([]);
+
+    useEffect( ()=>{
+        fetch('reviews.json')
+        .then(res=> res.json() )
+        .then(data => {
+           setReviews(data)
+        } )
+    } , [] )
+
+
+    return (
+        <div className='my-20' >
+            <SectionTitle
+                heading={'Testimonial'}
+                subHeading={'What Our Client Say'}
+              >
+
+            </SectionTitle>
+
+            <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+         {
+            reviews.map (review =>  <SwiperSlide key={review._id} >
+                <div className='m-10 flex flex-col items-center mx-24 my-16  ' >
+ 
+                <Rating
+      style={{ maxWidth: 180 }}
+      value={review.rating}
+      readOnly
+    />
+
+                    <p className='py-8' >{review.details}</p>
+                    <h2 className='text-2xl text-orange-400 text-center ' >{review.name}</h2>
+                </div>
+                </SwiperSlide>
+            )
+         }
+      </Swiper>
+
+        </div>
+    );
+};
+
+export default Testimonials
